e90440417b843c385fb7ee7e5bce4fe276d7455f
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-10-19 19:12:06 +0200

    even functions added

--- estado.c
+++ estado.c
@@ -6,52 +6,72 @@
 
 struct _Estado{
     char* nombre;
-    int tipo;    
+    int tipo;
 };
 
 Estado * estadoNuevo( char * nombre, int tipo){
     Estado *estado = NULL;
-    
+
     if(!nombre) return NULL;
-    
+
     estado = (Estado*)malloc(sizeof(Estado));
-    
+
     estado->nombre = (char*)malloc((strlen(nombre)+1)*sizeof(char));
     strcpy(estado->nombre, nombre);
-      
+
     estado->tipo = tipo;
-    
+
     return estado;
 }
 /* Reserva memoria nueva para el estado y para la cadena de su nombre, crea un
 estado con las dos componentes que se le proporcionan como argumento */
-void estadoElimina( Estado * p_s);
+void estadoElimina( Estado * p_s){
+    free(estado->nombre);
+    estado->nombre=NULL;
+    free(p_s);
+}
 /* Libera la memoria asociada con el estado */
 void estadoImprime( FILE * fd, Estado * p_s){
     if(!fd || !p_s) return;
-    
+
     switch(p_s->tipo){
         case INICIAL:
             break;
         case FINAL:
             break;
-        case INICIAL_Y_FINAL: 
+        case INICIAL_Y_FINAL:
             break;
         default:
             break;
-            
+
     }
 }
 /* Muestra por el FILE * el estado */
-int estadoEs(Estado * p_s, char * nombre);
-/* Función de comparación de estados por nombre, devuelve 1 si el estado tiene el
-nombre proporcionado */
+int estadoEs(Estado * p_s, char * nombre){
+  if(strcmp(p_s->nombre,nombre)){
+    return 1;
+  }else
+    return 0;
+}
 char * estadoNombre(Estado * p_s);
 /* Devuelve (un puntero) al nombre del estado */
-int estadoTipo(Estado * p_s);
-/* Devuelve el tipo de estado */
+int estadoTipo(Estado * p_s){
+  return p_s->tipo;
+}
 int estadoCompara(Estado * p_s1, Estado * p_s2);
 /* Se usa para comparar estados por su nombre y devuelve el strcmp de sus nombres
 */
-Estado * estado_copy(Estado * p_s);
-/* Se crea una copia en memoria nueva del estado proporcionado como argumento */
+Estado * estado_copy(Estado * p_s){
+  Estado *estado = NULL;
+
+  if (!p_s) return NULL;
+
+  estado = (Estado*)malloc(sizeof(Estado));
+
+  estado->nombre = (char*)malloc((strlen(p_s->nombre)+1)*sizeof(char));
+  strcpy(estado->nombre, p_s->nombre);
+
+  estado->tipo = p_s->tipo;
+
+  return estado;
+}
