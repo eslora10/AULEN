d25a7ad1b135d3b624767281210b6edadde22e08
Esther <esther.lopezramos@estudiante.uam.es>
2017-10-26 20:05:38 +0200

    capnd functions done. Need to test them

--- configuracionApnd.c
+++ configuracionApnd.c
@@ -23,7 +23,16 @@ ConfiguracionApnd * configuracionApndIni( ){
 }
 
 ConfiguracionApnd* configuracionApndInsert(ConfiguracionApnd* capnd, const
-ConfiguracionAp * p_cap);
+ConfiguracionAp * p_cap){
+    if(!capnd || !p_cap) return capnd;
+    
+    if(list_belongs(capnd->set, (void *) p_cap))
+        return capnd;
+    
+    capnd->set = list_insertFirst(capnd->set, (void *)p_cap);
+    
+    return capnd;
+}
 /* Inserta una configuración determinista en una no determinista. Se realiza una
 copia en memoria propia de la colección para el nuevo elemento y se asegura que
 no haya duplicados*/
@@ -37,13 +46,29 @@ ConfiguracionAp * configuracionApndExtract(ConfiguracionApnd * capnd){
 /* Se obtiene una configuración determinista de la no determinista, que
 desaparece de la colección.
 No se especifica el orden en el que se extrae */
-int configuracionApndIsEmpty(const ConfiguracionApnd* capnd);
+int configuracionApndIsEmpty(const ConfiguracionApnd* capnd){
+        if(!capnd) return -1;
+    
+    return list_isEmpty(capnd->set);
+}
 /* Se devuelve 1 si está vacía y 0 en caso contrario */
-int configuracionApndSize(const ConfiguracionApnd * capnd);
+int configuracionApndSize(const ConfiguracionApnd * capnd){
+    if(!capnd) return -1;
+    
+    return list_size(capnd->set);
+}
 /* Se devuelve el número de configuraciones deterministas que hay dentro de la no
 determinista */
-int configuracionApndPrint(FILE *fd, const ConfiguracionApnd* capnd);
+int configuracionApndPrint(FILE *fd, const ConfiguracionApnd* capnd){
+    if(!capnd || !fd) return -1;
+    
+    return list_print(fd, capnd->set);
+}
 /* Se imprime todas las configuraciones deterministas. No se especifica en qué
 orden */
-void configuracionApndDestroy( ConfiguracionApnd* capnd);
+void configuracionApndDestroy( ConfiguracionApnd* capnd){
+    if(!capnd) return;
+    
+    list_destroy(capnd->set);
+}
 /* Se libera toda la memoria asociada con la configuracion no determinista */
