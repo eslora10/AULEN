2fc1c5c194dbf065394c260c5356b21a8fe9bd2b
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-11-16 18:32:01 +0100

    AP.c added sin struct

---
+++ AP.c
@@ -0,0 +1,35 @@
+AP * APNuevo( char * nombre,
+              int num_estados,
+              int num_simbolos_entrada,
+              int num_simbolos_pila );
+
+void APElimina(AP * p_ap);
+
+void APImprime(FILE * fd, AP* p_ap);
+
+AP * APInsertaSimboloAlfabetoEntrada(AP * p_ap, char * simbolo);
+
+AP * APInsertaSimboloAlfabetoPila(AP * p_ap, char * simbolo);
+
+AP * APInsertaEstado(AP  * p_ap, char * nombre, int tipo);
+
+AP * APInsertaLTransicion(AP * p_ap,
+                          char * nombre_estado_i,
+                          char * nombre_estado_f );
+
+AP * APInsertaTransicion(	AP * p_ap,
+				char * nombre_simbolo_pila,
+				char * nombre_estado_i,
+				char * nombre_estado_f,
+				char * nombre_simbolo_entrada,
+				Palabra * accion);
+
+AP * APInsertaLetra(AP * p_ap, char * letra);
+
+AP * APCierraLTransicion (AP * p_ap);
+
+AP * APInicializaEstado (AP * p_ap);
+
+int APTransita(AP * p_ap);
+
+int APProcesaEntrada(FILE *fd, AP * p_ap);
