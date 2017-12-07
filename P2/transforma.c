5a81e2a4ec55d578d0791c4ffe9d55c4c9ff49a6
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-12-07 18:51:42 +0100

    Transforma in progress

--- P2/transforma.c
+++ P2/transforma.c
@@ -1,7 +1,20 @@
 
 
 AFND * AFNDTransforma(AFND * afnd){
+    AFND * afd;
+    int i,num_simbolos;
+    char* nombreIni;
+    num_simbolos=AFNDNumSimbolos(afnd);
 
+    afd= AFNDNuevo("finito",num_simbolos,AFNDNumEstados(afnd));
+
+    /*conseguir los simbolos paar el nuevo automata*/
+    for(i=0;i<num_simbolos;i++)
+        AFNDInsertaSimbolo(afd, AFNDSimboloEn(afnd, i));
+
+    /*conseguir el estado inicial*/
+    i=AFNDIndiceEstadoInicial(afnd);
+    nombreIni=AFNDNombreEstadoEn(afnd, i);
 
 
 
